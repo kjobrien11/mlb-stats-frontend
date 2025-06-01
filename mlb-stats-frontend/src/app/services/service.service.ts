1ef2cacb2236d54a2680aeb936e7cbb6530225a8
KJ OBrien <kjokjo219@gmail.com>
2025-06-01 14:11:26 -0400

    adds service t call backedn

---
+++ mlb-stats-frontend/src/app/services/service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  private apiUrl = 'http://127.0.0.1:8000/line-scores';
+
+  constructor(private http: HttpClient) {}
+
+  getLineScores(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
+
