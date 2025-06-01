7f45df504f19c24785142478f25df9b00e451918
KJ OBrien <kjokjo219@gmail.com>
2025-06-01 15:24:17 -0400

    adds button to start a live update feed

--- mlb-stats-frontend/src/app/line-score/line-score.component.ts
+++ mlb-stats-frontend/src/app/line-score/line-score.component.ts
@@ -10,14 +10,41 @@ import { ServiceService } from '../services/service.service';
 })
 export class LineScoreComponent {
 
-  scores: any[] = [];
+  scores: string[] = [];
+  current_score!: string 
+  curren_score_counter = 0;
 
   constructor(private scoreFetcher: ServiceService) {}
 
   ngOnInit(): void {
+    this.refreshLineScores();
+    this.current_score =  this.scores[0]
+    console.log(this.scores[0]);
+  }
+
+  refreshLineScores(){
+    console.log("getting new scores");
     this.scoreFetcher.getLineScores().subscribe((data) => {
       this.scores = data;
     });
-    console.log(this.scores[0])
   }
+
+  refreshCurrentScore(){
+    if(this.curren_score_counter +1 == this.scores.length){
+      this.refreshLineScores();
+      this.curren_score_counter = 0;
+    }else{
+      this.curren_score_counter = this.curren_score_counter +1;
+    }
+    this.current_score = this.scores[this.curren_score_counter];
+    console.log(this.current_score);
+  }
+
+  startTime(){
+    setInterval(() => {
+      this.refreshCurrentScore();
+    }, 1000); 
+  }
+
+
 }
