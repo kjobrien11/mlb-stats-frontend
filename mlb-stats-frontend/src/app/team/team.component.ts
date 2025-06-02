f6f90b1fd868ff2b1dcd65686eac6b69268f5181
KJ OBrien <kjokjo219@gmail.com>
2025-06-02 16:28:41 -0400

    fixes athletics name

--- mlb-stats-frontend/src/app/team/team.component.ts
+++ mlb-stats-frontend/src/app/team/team.component.ts
@@ -17,9 +17,10 @@ export class TeamComponent implements OnInit{
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['teamName']) {
       const currentTeam = changes['teamName'].currentValue;
-      console.log('Changes in TeamComponent:', changes);
       this.logo = this.generateImageName(currentTeam);
+      console.log(currentTeam);
     }
+   
   }
 
   ngOnInit(){
@@ -27,14 +28,44 @@ export class TeamComponent implements OnInit{
     console.log(this.logo)
   }
 
-  private generateImageName(team:string) {
-
-    const parts = team.trim().split(' ');
-    if (parts.length > 1) {
-      const fullTeamName = parts.slice(1).join(' ').toLowerCase();
-      return `${fullTeamName}.png`;
-    } else {
-      return 'NO_NAME.png';
-    }
+  private generateImageName(team: string): string {
+    const mapped = this.teamNameMap[team.trim()];
+    return mapped ? mapped : 'NO_NAME.png';
   }
+
+  private teamNameMap: { [key: string]: string } = {
+    'Arizona Diamondbacks': 'diamondbacks.png',
+    'Atlanta Braves': 'braves.png',
+    'Baltimore Orioles': 'orioles.png',
+    'Boston Red Sox': 'redsox.png',
+    'Chicago White Sox': 'whitesox.png',
+    'Chicago Cubs': 'cubs.png',
+    'Cincinnati Reds': 'reds.png',
+    'Cleveland Guardians': 'guardians.png',
+    'Colorado Rockies': 'rockies.png',
+    'Detroit Tigers': 'tigers.png',
+    'Houston Astros': 'astros.png',
+    'Kansas City Royals': 'royals.png',
+    'Los Angeles Angels': 'angels.png',
+    'Los Angeles Dodgers': 'dodgers.png',
+    'Miami Marlins': 'marlins.png',
+    'Milwaukee Brewers': 'brewers.png',
+    'Minnesota Twins': 'twins.png',
+    'New York Mets': 'mets.png',
+    'New York Yankees': 'yankees.png',
+    'Athletics': 'athletics.png',
+    'Philadelphia Phillies': 'phillies.png',
+    'Pittsburgh Pirates': 'pirates.png',
+    'San Diego Padres': 'padres.png',
+    'San Francisco Giants': 'giants.png',
+    'Seattle Mariners': 'mariners.png',
+    'St. Louis Cardinals': 'cardinals.png',
+    'Tampa Bay Rays': 'rays.png',
+    'Texas Rangers': 'rangers.png',
+    'Toronto Blue Jays': 'bluejays.png',
+    'Washington Nationals': 'nationals.png'
+  };
+  
+
+  
 }
