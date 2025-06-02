0932cc7ee5d7f2b7606731195329a353f855b81d
KJ OBrien <kjokjo219@gmail.com>
2025-06-02 16:40:58 -0400

    adds capability to pass data into score

--- mlb-stats-frontend/src/app/score/score.component.ts
+++ mlb-stats-frontend/src/app/score/score.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { LineScoreComponent } from '../line-score/line-score.component';
 import { LiveStatsComponent } from '../live-stats/live-stats.component';
 
@@ -9,6 +9,14 @@ import { LiveStatsComponent } from '../live-stats/live-stats.component';
   styleUrl: './score.component.css',
   standalone:true
 })
-export class ScoreComponent {
+export class ScoreComponent implements OnInit{
 
+  @Input() outs!: number;
+  @Input() inningStatus!: string;
+  @Input() base_occupancy: any;
+
+  ngOnInit(): void {
+    console.log(this.outs);
+    console.log(this.inningStatus);
+  }
 }
