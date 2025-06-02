86cab58f587e66386d06a5b29257b1ff94de1fa6
KJ OBrien <kjokjo219@gmail.com>
2025-06-02 15:46:09 -0400

    adds all team logos and some logic to display

--- mlb-stats-frontend/src/app/team/team.component.ts
+++ mlb-stats-frontend/src/app/team/team.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -8,8 +8,25 @@ import { NgIf } from '@angular/common';
   styleUrl: './team.component.css',
   standalone: true
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit{
   @Input() teamName!: string;
   @Input() score!:number;
   @Input() isHome!:boolean;
+  logo!: string;
+
+  ngOnInit(){
+    this.logo = this.generateImageName(this.teamName)
+    console.log(this.logo)
+  }
+
+  private generateImageName(team:string) {
+
+    const parts = team.trim().split(' ');
+    if (parts.length > 1) {
+      const fullTeamName = parts.slice(1).join(' ').toLowerCase();
+      return `${fullTeamName}.png`;
+    } else {
+      return 'NO_NAME.png';
+    }
+  }
 }
